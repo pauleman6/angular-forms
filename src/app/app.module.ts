c4cfb2645874ebfbe4f8a0e56d7a4e9778a2c479
pauleman6 <pauleman6@gmail.com>
2020-03-31 15:57:44 -0500

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { FeedbackFormComponent } from './feedback-form/feedback-form.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule ],
+  declarations: [ AppComponent, HelloComponent, FeedbackFormComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/feedback-form/feedback.ts
@@ -0,0 +1,13 @@
+export class Feedback{
+
+  firstName: string;
+  lastName: string;
+  phone: number;
+  email: string;
+  agree: boolean;
+  contacttype: string;
+  message: string;
+
+}
+
+export const contactType = ['None', 'Phone', 'Email'];
